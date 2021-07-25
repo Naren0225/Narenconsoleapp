6af67f76cc7662129993db4e947c8006c7427caa
Naren0225 <narendranaren881@gmail.com>
2021-07-25 17:30:26 +0530

    Add project files.

---
+++ Narenconsoleapp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Narenconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD!");
+            Console.WriteLine("CONSOLE APP!");
+
+
+            student std = new student();
+
+
+            std.name = "Naren";
+            std.studentId = 1;
+            std.usn = "1BSC42";
+
+            Console.WriteLine("name: "+ std.name);
+            Console.WriteLine("studentid: "+ std.studentId);
+            Console.WriteLine("usn: "+ std.usn);
+            Console.ReadKey();
+
+            /*
+            Console.WriteLine("using static method");
+            numeric.add(10, 3);*/
+
+
+            numeric numeric = new numeric();
+            Console.WriteLine("using non static method");
+            numeric.sum(20, 30);
+
+        }
+    }
+}
---
+++ Narenconsoleapp/numeric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Narenconsoleapp
+{
+    class numeric
+    {
+        public static void add(int a,int b)
+        {
+            Console.WriteLine("sum of two :" + (a + b));
+            Console.ReadKey();
+        }
+        public void  sum(int a,int b)
+        {
+            Console.WriteLine("sum of two :" + (a + b));
+            Console.ReadKey();
+        }
+    }
+}
